15fa1ecbf04ce1428c53fd5fbdb0dec0cca2e7f0
Anjali Kumari <81029259+Anjali972001@users.noreply.github.com>
2022-11-30 17:51:55 +0530

    new

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var div=document.createElement('div');
+    div.id='demo';
+    div.innerHTML='Hi !';
+    document.body.appendChild(div);
+
+},false);
